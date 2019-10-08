[WIP] Debugging KL divergence bug (not decreases!)

mnist_cnn.py
@@ -109,22 +109,22 @@ if __name__ == "__main__":
 
     y = y_test.argmax(axis=1)
 
-    sk_tSNE = TSNE(verbose=1, early_exaggeration=1.0)       # Disable "early-exaggeration" for a fair comparison!
+    sk_tSNE = TSNE(method='exact', verbose=2, early_exaggeration=1.0)       # Disable "early-exaggeration" for a fair comparison!
     embds = sk_tSNE.fit_transform(feats)
     #  Save output embds
     np.save('mnist_sk_feats_out.npy', embds)
     # Plot
     plot_mnist(embds, y, 'mnist_sk_feats_plot.png')
 
-    # Compute embeddings using ptSNE
-    from parametric_tsne import ParametricTSNE
+    # # Compute embeddings using ptSNE
+    # from parametric_tsne import ParametricTSNE
 
-    ptSNE = ParametricTSNE(verbose=1)
-    embds = ptSNE.fit_transform(feats)
-    #  Save output embds
-    np.save('mnist_ptsne_feats_out.npy', embds)
-    # Plot
-    plot_mnist(embds, y, 'mnist_ptsne_feats_plot.png')
+    # ptSNE = ParametricTSNE(verbose=1, n_iter=1000, logdir='tensorboard/mnist/1000')
+    # embds = ptSNE.fit_transform(feats)
+    # #  Save output embds
+    # np.save('mnist_ptsne_feats_out.npy', embds)
+    # # Plot
+    # plot_mnist(embds, y, 'mnist_ptsne_feats_plot.png')
 
     # # ---------------------- ADVERSARIAL EXAMPLES STARTS HERE! ----------------------
     # import sys